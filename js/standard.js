14ac44d51e0b665d5959c543cb796b0d1430acd1
mvitalia <p.guglielmo@mvitalia.com>
2015-07-31 16:37:26 +0200

    p

--- js/standard.js
+++ js/standard.js
@@ -36,22 +36,25 @@ $(document).ready(function () {
 
     });
 	
-	$("#txtAcconsentoInfo").click(function () {
-		var db = window.sqlitePlugin.openDatabase({name: "DatabaseProva1"});
-		db.transaction(function(tx) {
-            tx.executeSql("select count(id) as cnt from test_table;", [], function(tx, res) {
-              console.log("res.rows.length: " + res.rows.length + " -- should be 1");
-              console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
-            });
-          });
-	});
+	
 
    
        /* $("#form_richiestaInfo").validate();   
         $("#form-registrazioneAzienda").validate();*/
 
 
-    });
+});
+
+
+	window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(dir) {
+		console.log("got main dir",dir);
+		dir.getFile("log.txt", {create:true}, function(file) {
+			console.log("got the file", file);
+			logOb = file;
+			writeLog("App started");			
+		});
+	});
+
 
     function validateForm() {
 
@@ -119,46 +122,6 @@ $(document).ready(function () {
             return false;
         }
     }
-
-
-// Cordova is ready
-/*document.addEventListener("deviceready", onDeviceReady, false);
-
-// Cordova is ready
-function onDeviceReady() {
-  var db = window.sqlitePlugin.openDatabase({name: "my.db"});
-
-  db.transaction(function(tx) {
-    tx.executeSql('DROP TABLE IF EXISTS test_table');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
-
-    // demonstrate PRAGMA:
-    db.executeSql("pragma table_info (test_table);", [], function(res) {
-      console.log("PRAGMA res: " + JSON.stringify(res));
-    });
-
-    tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
-      console.log("insertId: " + res.insertId + " -- probably 1");
-      console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
-
-      db.transaction(function(tx) {
-        tx.executeSql("select count(id) as cnt from test_table;", [], function(tx, res) {
-          console.log("res.rows.length: " + res.rows.length + " -- should be 1");
-          console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
-        });
-      });
-
-    }, function(e) {
-      console.log("ERROR: " + e.message);
-    });
-  });
-}*/
-	
-	
-    
-
-
-
 
 
 
