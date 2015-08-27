45600f026daf40e227ae614c4a910d8bcae2d6e4
mvitalia <p.guglielmo@mvitalia.com>
2015-08-27 10:50:10 +0200

    a

--- js/standard.js
+++ js/standard.js
@@ -229,33 +229,16 @@ function caricaMappa(coordinate,nomeAzienda) {
     }
 
     function checkInternet() {
-        /*var online = window.navigator.onLine;
-        if (!online) {
-            return false;
-        } else {
-            return true;
-        }*/
-		
-			var networkState = navigator.connection.type;
-
-			var states = {};
-			states[Connection.UNKNOWN]  = false;
-			states[Connection.ETHERNET] = true;
-			states[Connection.WIFI]     = true;
-			states[Connection.CELL_2G]  = true;
-			states[Connection.CELL_3G]  = true;
-			states[Connection.CELL_4G]  = true;
-			states[Connection.CELL]     = true;
-			states[Connection.NONE]     = false;
-			
-			
-			
-			if (states[networkState]) {
-				
-				return true;
-			} else {
-				return false;
-			}
+		var rete;
+        if(navigator.network.connection.type == Connection.NONE){
+                alert("nocon");
+				rete = false;
+				return rete;
+            }else{
+                alert("yescon");
+				rete = true;
+				return rete;
+            }
 			
     }
 
