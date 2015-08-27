e18457d51efa2f13d2f8f1f6be5541490f024db7
mvitalia <p.guglielmo@mvitalia.com>
2015-08-27 14:44:15 +0200

    a

--- js/standard.js
+++ js/standard.js
@@ -240,23 +240,25 @@ function caricaMappa(coordinate,nomeAzienda) {
 				rete = true;
 				return rete;
             }*/
-		 var networkState;
-    networkState = navigator.network.connection.type;
-    var states = {};
-    states[Connection.UNKNOWN] = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI] = 'WiFi connection';
-    states[Connection.CELL_2G] = 'Cell 2G connection';
-    states[Connection.CELL_3G] = 'Cell 3G connection';
-    states[Connection.CELL_4G] = 'Cell 4G connection';
-    states[Connection.NONE] = 'No network connection';
-    if (states[networkState] != Connection.UNKNOWN || states[networkState] != Connection.NONE) {
-       rete = true;
-	   return rete;
-    } else {
-         rete = false;
-	   return rete;
-    }
+		var networkState;
+		networkState = navigator.network.connection.type;
+		var states = {};
+		states[Connection.UNKNOWN] = 'Unknown connection';
+		states[Connection.ETHERNET] = 'Ethernet connection';
+		states[Connection.WIFI] = 'WiFi connection';
+		states[Connection.CELL_2G] = 'Cell 2G connection';
+		states[Connection.CELL_3G] = 'Cell 3G connection';
+		states[Connection.CELL_4G] = 'Cell 4G connection';
+		states[Connection.NONE] = 'No network connection';
+		if (states[networkState] != Connection.UNKNOWN || states[networkState] != Connection.NONE) {
+			alert("true");
+		   rete = true;
+		   return rete;
+		} else {
+			alert("false");
+			 rete = false;
+		   return rete;
+		}
 			
     }
 
