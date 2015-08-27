9192d94aced9c30ad44c41a1bf78d7658130ffea
mvitalia <p.guglielmo@mvitalia.com>
2015-08-27 14:58:04 +0200

    a

--- js/standard.js
+++ js/standard.js
@@ -242,15 +242,15 @@ function caricaMappa(coordinate,nomeAzienda) {
             }*/
 		var networkState;
 		networkState = navigator.network.connection.type;
-		var states = {};
+		/*var states = {};
 		states[Connection.UNKNOWN] = 'Unknown connection';
 		states[Connection.ETHERNET] = 'Ethernet connection';
 		states[Connection.WIFI] = 'WiFi connection';
 		states[Connection.CELL_2G] = 'Cell 2G connection';
 		states[Connection.CELL_3G] = 'Cell 3G connection';
 		states[Connection.CELL_4G] = 'Cell 4G connection';
-		states[Connection.NONE] = 'No network connection';
-		if (states[networkState] != Connection.UNKNOWN || states[networkState] != Connection.NONE) {
+		states[Connection.NONE] = 'No network connection';*/
+		if (states[networkState] != Connection.UNKNOWN && states[networkState] != Connection.NONE) {
 			alert("true");
 		   rete = true;
 		   return rete;
